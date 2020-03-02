a01620a783bd5d6f8d5472baa12449a4a389cad9
Joe Dailey <josephdailey@gmail.com>
2020-03-02 18:46:57 +0000

    Add Debug Logs to Update.sh

--- server/update.sh
+++ server/update.sh
@@ -7,15 +7,24 @@
 #   0 */15 * ? * * sh /home/admin/delta.khk.org/server/update.sh /home/admin/delta.khk.org /var/www/delta.khk.org
 
 if [[ $# -eq 0 ]] ; then
-    printf "\n\n[$(date)]Jekyll Build Failed: No path to Jekyll site supplied.\n"
+    printf "\n[delta.khk.org| $(date)] Update Failed: Missing path(s)"
+    printf "\n\tUsage: update.sh <site path> <output path>"
+    printf "\n"
     exit 1
 fi
 
+printf "\n[delta.khk.org|$(date)] Update Started: `cd $1`\n"
 cd $1
+
+printf "[delta.khk.org|$(date)] Pull Started: `git checkout master && git pull`\n"
 git checkout master
 git pull
+
+printf "[delta.khk.org|$(date)] Dependency Update Started: `bundle install`\n"
 bundle install
+
+printf "[delta.khk.org|$(date)] Jekyll Build Started: `bundle exec jekyll build -d $2`\n"
 bundle exec jekyll build -d $2
 
-printf "\n\n[$(date)] \tJekyll Build: $1 > $2\n"
+printf "[delta.khk.org|$(date)] Update Deployed: $1 > $2\n"
 exit 0;
