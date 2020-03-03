ea7b6f73f07bb35cf82d54e6bc0507d06119e50b
Joe Dailey <josephdailey@gmail.com>
2020-03-03 06:19:19 +0000

    Add Logging to Cronjob

--- server/update.sh
+++ server/update.sh
@@ -4,27 +4,27 @@
 #
 # For example, this crontab entry will build the site every 15 minutes.
 #
-#   0 */15 * ? * * sh /home/admin/delta.khk.org/server/update.sh /home/admin/delta.khk.org /var/www/delta.khk.org
+#   */15 * * * * systemd-cat -t "delta.khk.org" /home/admin/delta.khk.org/server/update.sh /home/admin/delta.khk.org /var/www/delta.khk.org
 
 if [[ $# -eq 0 ]] ; then
-    printf "\n[delta.khk.org| $(date)] Update Failed: Missing path(s)"
+    printf "\n[$(date)] Update Failed: Missing path(s)"
     printf "\n\tUsage: update.sh <site path> <output path>"
     printf "\n"
     exit 1
 fi
 
-printf "\n[delta.khk.org|$(date)] Update Started: `cd $1`\n"
+printf "\n[$(date)] Update Started: `cd $1`\n"
 cd $1
 
-printf "[delta.khk.org|$(date)] Pull Started: `git checkout master && git pull`\n"
+printf "[$(date)] Pull Started: `git checkout master && git pull`\n"
 git checkout master
 git pull
 
-printf "[delta.khk.org|$(date)] Dependency Update Started: `bundle install`\n"
+printf "[$(date)] Dependency Update Started: `bundle install`\n"
 bundle install
 
-printf "[delta.khk.org|$(date)] Jekyll Build Started: `bundle exec jekyll build -d $2`\n"
+printf "[$(date)] Jekyll Build Started: `bundle exec jekyll build -d $2`\n"
 bundle exec jekyll build -d $2
 
-printf "[delta.khk.org|$(date)] Update Deployed: $1 > $2\n"
+printf "[$(date)] Update Deployed: $1 > $2\n"
 exit 0;
